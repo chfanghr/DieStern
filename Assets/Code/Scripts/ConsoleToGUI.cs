b5f556557cc3b06cee46db67bf8fbe8fa8177ae4
fanghr <chfanghr@gmail.com>
2020-10-23 06:43:48 +0800

    code: enhance debug facilities
    ci skip

--- Assets/Code/Scripts/ConsoleToGUI.cs
+++ Assets/Code/Scripts/ConsoleToGUI.cs
@@ -4,7 +4,6 @@ namespace Code.Scripts
 {
     public class ConsoleToGUI : MonoBehaviour
     {
-        static string _myLog = "";
         private string _output;
         private string _stack;
 
@@ -20,24 +19,25 @@ namespace Code.Scripts
  
         public void Log(string logString, string stackTrace, LogType type)
         {
-            if (type == LogType.Log)
-            {
-                return;
-            }
-            _output = logString;
+            // if (type == LogType.Log)
+            // {
+            //     return;
+            // }
+            _output = $"[{type}] {logString}\n";
             _stack = stackTrace;
-            _myLog = _output + "\n" + _myLog;
-            if (_myLog.Length > 5000)
-            {
-                _myLog = _myLog.Substring(0, 4000);
-            }
         }
 
+        public int fontSize = 20;
         private void OnGUI()
         {
-            var style = new GUIStyle {fontSize = 16};
-            _myLog = GUI.TextArea(
-                new Rect(10, 10, Screen.width/2 - 10, Screen.height/2 - 10), _myLog, style);
+            var style = new GUIStyle {fontSize = fontSize};
+            GUI.Label(
+                new Rect(10, 10, Screen.width/2 - 10, Screen.height/2 - 10), _output, style);
+        }
+
+        public void IncreaseFontSize(int by = 1)
+        {
+            fontSize += by;
         }
     }
 }
--- Assets/Code/Scripts/Experiments/ExperimentsEntry.cs
+++ Assets/Code/Scripts/Experiments/ExperimentsEntry.cs
@@ -7,11 +7,14 @@ namespace Code.Scripts.Experiments
 {
     public class ExperimentsEntry : MonoBehaviour
     {
+        public Canvas mainUICanvas;
         public Dropdown scenesMenu;
         public List<string> scenes;
-
+        public ConsoleToGUI debugLog;
+        
         private void Start()
         {
+            Debug.Log($"available scenes: {scenes}");
             scenesMenu.AddOptions(scenes);
             scenesMenu.value = 0;
         }
@@ -23,14 +26,22 @@ namespace Code.Scripts.Experiments
             ReturnMainScene();
             _activatedSceneName = scenesMenu.captionText.text;
             SceneManager.LoadScene(_activatedSceneName, LoadSceneMode.Additive);
+            Debug.Log($"{_activatedSceneName} loaded");
         }
 
         public void ReturnMainScene()
         {
             if(_activatedSceneName.Length>0){
+                Debug.Log($"{_activatedSceneName} unloaded");
                 SceneManager.UnloadSceneAsync(_activatedSceneName);
             }
             _activatedSceneName = "";
+        }
+
+        public void ToggleUI()
+        {
+            mainUICanvas.enabled = !mainUICanvas.enabled;
+            debugLog.enabled = !debugLog.enabled;
         }
     }
 }
